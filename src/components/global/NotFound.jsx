6cb9959d24e3ac8758e3a0e50bba07047263e9d1
Pradeep K <pradeepksept2000@gmail.com>
2023-10-30 20:53:10 +0530

    Updated changes

--- src/components/global/NotFound.jsx
+++ src/components/global/NotFound.jsx
@@ -1,15 +1,5 @@
 import React from "react";
 
-import html from "../../components/constants/Icons/html5.png";
-import css from "../../components/constants/Icons/css3.png";
-import reactjs from "../../components/constants/Icons/react.png";
-import csharp from "../../components/constants/Icons/c#.png";
-import aspCore from "../../components/constants/Icons/asp.png";
-import sql from "../../components/constants/Icons/sql.png";
-import git from "../../components/constants/Icons/git.png";
-import postman from "../../components/constants/Icons/postman.png";
-
-
 const NotFound =()=>{
     return(
         <div className="not-found">
